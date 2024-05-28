44e384d5b8bba056f7fb6ce8441f6be0e2725664
liliya <liliya817171@gmail.com>
2024-05-28 14:28:42 +0900

    [update] google analyticsを調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: "./dist",
+  outDir: './dist',
   css: {
-    preprocessor: "sass",
+    preprocessor: 'sass',
   },
   integrations: [
     react({
       experimentalReactChildren: true,
+    }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
     }),
   ],
 });
